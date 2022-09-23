f9957733284ae886e155bf146363e4589c442794
August Fredriksen <afr067@uit.no>
2022-09-23 14:38:12 +0200

    Yes it works

--- app/src/main/java/com/example/stroll/MainActivity.kt
+++ app/src/main/java/com/example/stroll/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 
+//It works!!!!!!!!
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
