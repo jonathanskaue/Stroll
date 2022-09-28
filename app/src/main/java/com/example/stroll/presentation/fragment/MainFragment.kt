0ef5242bda4a8a1b6415e646d085aa62b95b1261
Freddy <frederic.charon@gmx.de>
2022-09-28 12:54:54 +0200

    display accelerometer data

--- app/src/main/java/com/example/stroll/presentation/fragment/MainFragment.kt
+++ app/src/main/java/com/example/stroll/presentation/fragment/MainFragment.kt
@@ -1,10 +1,17 @@
 package com.example.stroll.presentation.fragment
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.viewModels
 import com.example.stroll.R
 import com.example.stroll.databinding.FragmentMainBinding
@@ -12,12 +19,14 @@ import com.example.stroll.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainFragment() : Fragment() {
+class MainFragment() : Fragment(), SensorEventListener {
 
     private val viewModel: MainViewModel by viewModels()
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var sensorManager: SensorManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,7 +37,38 @@ class MainFragment() : Fragment() {
         viewModel.addDataToRoom()
         viewModel.allData.observe(viewLifecycleOwner) { data ->
             binding.textView.text = data[0].id.toString()
+
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpSensors()
+    }
+
+    // outsource function to another class
+    private fun setUpSensors() {
+
+        val sensorManager = activity?.getSystemService(AppCompatActivity.SENSOR_SERVICE) as SensorManager
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            val sides = event.values[0]
+            val upDown = event.values[1]
+
+            binding.tvSensorData.text = "up/down ${upDown} \n sides ${sides}"
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
     }
 }
