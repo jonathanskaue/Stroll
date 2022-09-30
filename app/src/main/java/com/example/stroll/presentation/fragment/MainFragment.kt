a4752b8c60737eeb6e63bef2b7a759cc9a22d66a
Freddy <frederic.charon@gmx.de>
2022-09-30 11:56:51 +0200

    added rotationMatrix and orientation

--- app/src/main/java/com/example/stroll/presentation/fragment/MainFragment.kt
+++ app/src/main/java/com/example/stroll/presentation/fragment/MainFragment.kt
@@ -11,16 +11,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.viewModels
 import com.example.stroll.R
 import com.example.stroll.databinding.FragmentMainBinding
 import com.example.stroll.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
-import java.util.Vector
 import kotlin.concurrent.fixedRateTimer
 import kotlin.math.sqrt
 
@@ -34,8 +30,11 @@ class MainFragment() : Fragment(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
     private var sensorAccChange = floatArrayOf(0f, 0f, 0f)
+    private var accSensorData = floatArrayOf(0f, 0f, 0f)
     private var sensorGyroChange = listOf(0, 0, 0)
     private var sensorMagData = floatArrayOf(0f, 0f, 0f)
+    private var rotationMatrix = floatArrayOf(0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f)
+    private var deviceOrientation = floatArrayOf(0f, 0f, 0f)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,8 +48,10 @@ class MainFragment() : Fragment(), SensorEventListener {
         }
         setUpSensors()
 
-        fixedRateTimer("timer", false, 0, 2000){
+        fixedRateTimer("timer", false, 0, 500){
             Log.d("changesAcc", listOf(sensorAccChange[0], sensorAccChange[1], sensorAccChange[2]).toString())
+
+            // resetting sensorAccChange
             sensorAccChange = floatArrayOf(0f, 0f, 0f)
 
 
@@ -58,23 +59,17 @@ class MainFragment() : Fragment(), SensorEventListener {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-
-    }
-
-    // outsource function to another class
     private fun setUpSensors() {
         val sensorManager =
             activity?.getSystemService(AppCompatActivity.SENSOR_SERVICE) as SensorManager
-        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+        val sensorAcc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
             sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
         }
         sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)?.also {
             sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
         }
-        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also {
+        val sensorMag = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also {
             sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
 
         }
@@ -84,13 +79,14 @@ class MainFragment() : Fragment(), SensorEventListener {
     override fun onSensorChanged(event: SensorEvent?) {
 
         if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
-            val accSensorData = event.values
+
+            accSensorData = event.values
             val accTotal =
                 sqrt(accSensorData[0] * accSensorData[0] + accSensorData[1] * accSensorData[1] + accSensorData[2] * accSensorData[2]) - 9.81
             if (accTotal > 0.5) {
                 Log.d("changesAcc", listOf(accSensorData[0], accSensorData[1],accSensorData[2]).toString())
                 for (i in sensorAccChange.indices){
-                    sensorAccChange[i] = sensorAccChange[i] + accSensorData[i]
+                    sensorAccChange[i] += accSensorData[i]
                 }
 
             }
@@ -125,6 +121,13 @@ class MainFragment() : Fragment(), SensorEventListener {
                             )
                         }"
         }
+
+        SensorManager.getRotationMatrix(rotationMatrix, null, accSensorData, sensorMagData)
+        SensorManager.getOrientation(rotationMatrix, deviceOrientation)
+        binding.tvOrientation.text = "Device Orientation: ${(deviceOrientation[0]*180/3.14159).toInt()}," +
+                " ${(deviceOrientation[1]*180/3.14159).toInt()}, ${(deviceOrientation[2]*180/3.14159).toInt()}"
+
+        // binding.tvBox.apply { translationX = - (deviceOrientation[0]*180/3.14159f) * 20f }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
