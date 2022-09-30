4ae25fc69dc5923b58540245f31344ff5134cbbf
Skilldude7 <jonathan@skaue.com>
2022-09-30 14:21:03 +0200

    Now dark and light mode changes in real time at settings fragment

--- app/src/main/java/com/example/stroll/presentation/fragment/SettingsFragment.kt
+++ app/src/main/java/com/example/stroll/presentation/fragment/SettingsFragment.kt
@@ -1,7 +1,9 @@
 package com.example.stroll.presentation.fragment
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.example.stroll.MainActivity
@@ -11,5 +13,21 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+        loadSettings()
+        return super.onPreferenceTreeClick(preference)
+    }
+
+    fun loadSettings() {
+        val sp = context?.let { PreferenceManager.getDefaultSharedPreferences(it) }
+        val dark_mode = sp?.getBoolean("dark_mode", false)
+
+        if (dark_mode == true) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
