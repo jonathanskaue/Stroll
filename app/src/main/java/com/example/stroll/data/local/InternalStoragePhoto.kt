0edd92313a7e1a1ae2d8915301e7d55964c3b682
Skilldude7 <jonathan@skaue.com>
2022-11-11 20:35:57 +0100

    Add PhotoAdapter and use it in hikedetails and show pictures from given hike

---
+++ app/src/main/java/com/example/stroll/data/local/InternalStoragePhoto.kt
@@ -0,0 +1,8 @@
+package com.example.stroll.data.local
+
+import android.graphics.Bitmap
+
+data class InternalStoragePhoto(
+    val name: String,
+    val bmp: Bitmap
+)
--- app/src/main/java/com/example/stroll/presentation/fragment/HikeDetailsFragment.kt
+++ app/src/main/java/com/example/stroll/presentation/fragment/HikeDetailsFragment.kt
@@ -1,5 +1,8 @@
 package com.example.stroll.presentation.fragment
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,11 +13,20 @@ import com.example.stroll.databinding.FragmentHomeBinding
 
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.stroll.data.local.InternalStoragePhoto
 import com.example.stroll.databinding.FragmentHikeDetailsBinding
 import com.example.stroll.other.Utility
+import com.example.stroll.presentation.adapters.PhotoAdapter
 import com.example.stroll.presentation.viewmodel.StatisticsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
 import java.lang.Math.round
 import java.util.*
 import javax.inject.Inject
@@ -26,6 +38,7 @@ class HikeDetailsFragment : BaseFragment() {
     var name = ""
 
     val args: HikeDetailsFragmentArgs by navArgs()
+    private var photoAdapter = PhotoAdapter()
 
     private val viewModel: StatisticsViewModel by viewModels()
 
@@ -39,7 +52,8 @@ class HikeDetailsFragment : BaseFragment() {
 
         // Inflate the layout for this fragment
         _binding = FragmentHikeDetailsBinding.inflate(inflater, container, false)
-
+        setUpPhotoRecyclerView()
+        loadHikePhotosIntoRecyclerView()
         return binding.root
     }
 
@@ -47,5 +61,30 @@ class HikeDetailsFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
         Log.d("args", "onViewCreated: $args")
 
+    }
+
+    private fun setUpPhotoRecyclerView() = binding.rvPrivatePhotos.apply {
+        adapter = photoAdapter
+        layoutManager = StaggeredGridLayoutManager(3, RecyclerView.VERTICAL)
+    }
+
+    private fun loadHikePhotosIntoRecyclerView(){
+        lifecycleScope.launch{
+            val photos = loadHikePhotos()
+            photoAdapter.submitList(photos)
+        }
+    }
+
+    private suspend fun loadHikePhotos(): List<InternalStoragePhoto>{
+        return withContext(Dispatchers.IO) {
+            val path = context?.filesDir?.absolutePath + "/${args.id}/"
+            val dir = File(path).listFiles()
+            Log.d("dir", "${dir?.toList()}: ")
+            dir.filter { it.canRead() && it.isFile && it.name.endsWith(".png") }!!.map {
+                val bytes = it.readBytes()
+                val bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                InternalStoragePhoto(it.name, bmp)
+            }
+        }
     }
 }
