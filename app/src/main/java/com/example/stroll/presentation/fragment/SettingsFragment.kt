9020cbd5b41d6aa017d8fe856a2d2a9f7f0cf24e
August Fredriksen <afr067@uit.no>
2022-12-02 22:04:06 +0100

    Fix settings bug

--- app/src/main/java/com/example/stroll/presentation/fragment/SettingsFragment.kt
+++ app/src/main/java/com/example/stroll/presentation/fragment/SettingsFragment.kt
@@ -18,6 +18,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        loadSettings()
 
     }
 
